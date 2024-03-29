f381d2bbac485f78090514a77591b723d3c167d4
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-03-29 10:34:19 +0900

    Title: 세 실수의 반올림, Time: 170ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240329/세 실수의 반올림/rounding-of-three-actual-numbers.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextDouble()
+    val b = sc.nextDouble()
+    val c = sc.nextDouble()
+
+    println("%.3f".format(a))
+    println("%.3f".format(b))
+    println("%.3f".format(c))
+}
