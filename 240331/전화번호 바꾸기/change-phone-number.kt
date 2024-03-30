e03fdfc0d33b3a1405dd686f21bd9198a86b4f7e
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-03-31 01:13:04 +0900

    Title: 전화번호 바꾸기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240331/전화번호 바꾸기/change-phone-number.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    sc.useDelimiter("-")
+    val a = sc.next()
+    val b = sc.next()
+    val c = sc.next()
+
+    print("$a-$c-$b")
+}
