7a74931ee63efa2cef92fda18ab70a2dbfe1e368
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-01 00:34:48 +0900

    Title: 큰 수에서 빼기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240401/큰 수에서 빼기/subtract-from-large-number.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    if (a > b) {
+        print(a - b)
+    } else {
+        print(b - a)
+    }
+}
