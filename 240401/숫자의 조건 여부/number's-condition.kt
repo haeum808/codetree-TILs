624fb7fe2610b5444b66dbcaaa4c29fdbb455803
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-01 00:41:09 +0900

    Title: 숫자의 조건 여부, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240401/숫자의 조건 여부/number's-condition.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+
+    if (a >= 113) {
+        println(1)
+    } else {
+        println(0)
+    }
+}
