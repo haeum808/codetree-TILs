c2014446806e6c84f268cf3b621ec37fd7605299
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-03-31 14:18:28 +0900

    Title: 합과 차의 나눗셈, Time: 171ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240331/합과 차의 나눗셈/divide-of-sum-and-sub.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextDouble()
+    val b = sc.nextDouble()
+
+    print("%.2f".format((a + b) / (a - b)))
+}
