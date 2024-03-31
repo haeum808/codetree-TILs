bfb5653f63bfb0bc555d0c3696091e5ba50568aa
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-03-31 14:01:51 +0900

    Title: 입력받아 연산하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240331/입력받아 연산하기/take-input-and-operate.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    println("${a + 87}")
+    println("${b % 10}")
+}
