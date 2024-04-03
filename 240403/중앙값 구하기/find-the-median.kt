e39dbc2e072d438f219077f53f77348f01c2415c
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-03 22:58:55 +0900

    Title: 중앙값 구하기, Time: 185ms, Memory: 17MB, Status: Passed - CodeTree

--- 240403/중앙값 구하기/find-the-median.kt
+++ 240403/중앙값 구하기/find-the-median.kt
@@ -3,7 +3,7 @@ import java.util.Scanner
 fun main() {
     val sc = Scanner(System.`in`)
 
-    val numbers = sc.nextLine().trim().split(" ").sorted()
+    val numbers = sc.nextLine().trim().split(" ").map { it.toInt() }.sorted()
 
     print(numbers[1])
 }
