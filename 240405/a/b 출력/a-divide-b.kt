6530d1319550f1f979148a5dcd37419fa62322c4
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-05 00:13:21 +0900

    Title: a/b 출력, Time: 150ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240405/a/b 출력/a-divide-b.kt
+++ 240405/a/b 출력/a-divide-b.kt
@@ -3,12 +3,7 @@ import java.util.Scanner
 fun main() {
     val sc = Scanner(System.`in`)
 
-    var (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
+    var (a, b) = sc.nextLine().trim().split(" ").map { it.toDouble() }
 
-    print(a / b)
-    print(".")
-    repeat(20) {
-        print("${a * 10 / b}")
-        a = a * 10 % b
-    }
+    print("%.20f".format(a / b))
 }
