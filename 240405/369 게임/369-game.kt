f67da6fd0eb52e75fbe7c88091ab5dbffdc8e91d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-05 23:30:29 +0900

    Title: 369 게임, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240405/369 게임/369-game.kt
+++ 240405/369 게임/369-game.kt
@@ -6,7 +6,7 @@ fun main() {
     val n = sc.nextInt()
 
     for (i in 1..n) {
-        if (i.toString().any { it == '3' || it == '6' || it == '9'}) {
+        if (i.toString().any { it == '3' || it == '6' || it == '9'} || i % 3 == 0) {
             print("0 ")
         }
         else {
