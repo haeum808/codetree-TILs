61239297f2b41ec7bd2e755a1823a4c7f3aa5328
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-05 23:28:58 +0900

    Title: 369 게임, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240405/369 게임/369-game.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        if (i.toString().any { it == '3' || it == '6' || it == '9'}) {
+            print("0 ")
+        }
+        else {
+            print("$i ")
+        }
+    }
+}
