d691dc269fa4fe55f7d944bda9c38d1875262f35
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-05 23:07:47 +0900

    Title: a/b 출력, Time: 169ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240405/a/b 출력/a-divide-b.kt
+++ 240405/a/b 출력/a-divide-b.kt
@@ -6,6 +6,11 @@ fun main() {
     var (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
 
     print("${a / b}.")
+
+    if (a / b >= 1) {
+        a = a * 10 % b
+    }
+
     repeat(20) {
         print("${a * 10 / b}")
         a = a * 10 % b
