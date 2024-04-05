7d256f4092d4d0464d4227829539e800725bef7d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-05 23:18:22 +0900

    Title: 2 또는 3의 배수, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240405/2 또는 3의 배수/multiple-of-2-or-3.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        if (i % 2 == 0 || i % 3 == 0) print("1 ")
+        else print("0 ")
+    }
+}
