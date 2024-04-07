af3c0d614922c8b2b12dd91347fe74d0b0f7e1cb
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-07 20:27:03 +0900

    Title: 주어진 수까지의 곱, Time: 170ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240407/주어진 수까지의 곱/multiplication-up-to-a-given-number.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
+
+    print((a..b).fold(1) { acc, value -> acc * value })
+}
