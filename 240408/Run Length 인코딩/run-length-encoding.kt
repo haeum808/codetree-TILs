51453802f1ed950984f57018503eb7c8c3437666
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-08 23:39:09 +0900

    Title: Run Length 인코딩, Time: 146ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240408/Run Length 인코딩/run-length-encoding.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val input = sc.nextLine()
+    val sb = StringBuilder()
+
+    if (input.length == 1) {
+        print(2)
+        print("${input}1")
+    } else {
+        var count = 1
+        for (i in 1..input.length - 1) {
+            if (input[i] == input[i - 1]) {
+                count++
+            } else {
+                if (i == 1) sb.append("${input[i - 1]}1")
+                sb.append("${input[i - 1]}$count")
+                count = 1
+            }
+        }
+        sb.append("${input.last()}$count")
+
+        println(sb.length)
+        println(sb)
+    }
+}
