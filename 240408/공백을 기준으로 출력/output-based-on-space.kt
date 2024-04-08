70559cbe5bad8ae022a45959dcc547b3b4f82440
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-08 23:03:57 +0900

    Title: 공백을 기준으로 출력, Time: 339ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240408/공백을 기준으로 출력/output-based-on-space.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    
+    val input1 = sc.nextLine().replace(" ", "")
+    val input2 = sc.nextLine().replace(" ", "")
+
+    print("$input1$input2")
+}
