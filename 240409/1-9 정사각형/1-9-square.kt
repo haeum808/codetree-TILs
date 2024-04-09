2aa1f6029f796e9be7297274b8449635a25d8f53
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-09 20:43:26 +0900

    Title: 1-9 정사각형, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/1-9 정사각형/1-9-square.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    var cnt = 1
+
+    for (i in 1..n) {
+        for (j in 1..n) {
+            print("${cnt++}")
+
+            if (cnt == 10) cnt = 1
+        }
+        println()
+    }
+}
