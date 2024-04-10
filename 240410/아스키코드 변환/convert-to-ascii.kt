0305267432005932f9a0329cae679929308577b8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-10 19:19:48 +0900

    Title: 아스키코드 변환, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240410/아스키코드 변환/convert-to-ascii.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+import kotlin.math.abs
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.next()[0]
+    val b = sc.nextInt()
+
+    print("${a.toInt()} ${b.toChar()}")
+}
