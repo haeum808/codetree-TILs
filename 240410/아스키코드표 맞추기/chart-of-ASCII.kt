a5d5c18b54514e3339f3915b9fb51abea6546543
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-10 19:18:56 +0900

    Title: 아스키코드표 맞추기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240410/아스키코드표 맞추기/chart-of-ASCII.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+import kotlin.math.abs
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val c = sc.nextInt()
+    val d = sc.nextInt()
+    val e = sc.nextInt()
+
+    print("${a.toChar()} ${b.toChar()} ${c.toChar()} ${d.toChar()} ${e.toChar()}")
+}
