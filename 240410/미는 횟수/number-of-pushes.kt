27063c3a1729b5e59a0729c4f6f4bb0819f6b481
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-10 20:07:31 +0900

    Title: 미는 횟수, Time: 156ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240410/미는 횟수/number-of-pushes.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    var a = sc.next()
+    val b = sc.next()
+    var cnt = 0
+
+    while(cnt <= a.length) {
+        if (a == b) break
+
+        a = a.slice(1..a.length - 1) + a[0]
+
+        cnt++
+    }
+
+    if (cnt > a.length) {
+        print(-1)
+    } else {
+        print(cnt)
+    }
+}
