d22364b28e935ab4e31710b6348281ee15513468
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 21:15:51 +0900

    Title: 두 번째로 작은 수의 위치, Time: 206ms, Memory: 15MB, Status: Passed - CodeTree

--- 240411/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.kt
+++ 240411/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.kt
@@ -1,27 +1,23 @@
 import java.util.Scanner
-import kotlin.math.min
 
 fun main() {
     val sc = Scanner(System.`in`)
-
     val n = sc.nextInt()
     sc.nextLine()
     val numbers = sc.nextLine().trim().split(" ").map { it.toInt() }
-
-    var min = numbers.min()!!
-
-    for (number in numbers) {
-        if (number > min) {
-            min = number
-
-            if (numbers.indexOf(min) == numbers.lastIndexOf(min)) {
-                print(numbers.indexOf(min) + 1)
-            } else {
-                print(-1)
-            }
-            return
-        }
+    val set = numbers.toSortedSet()
+    var secondMin = 0
+    
+    if (set.size == 1) {
+        print(-1)
+        return
+    } else {
+        secondMin = set.take(2)[1]
     }
-
-    print(-1)
+    
+    if (numbers.indexOf(secondMin) == numbers.lastIndexOf(secondMin)) {
+        print(numbers.indexOf(secondMin) + 1)
+    } else {
+        print(-1)
+    }
 }
