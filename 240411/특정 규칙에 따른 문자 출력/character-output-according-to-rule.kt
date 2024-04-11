43a658254ca9bfa9cd9b91bc769a63ba8436505a
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 13:45:09 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/특정 규칙에 따른 문자 출력/character-output-according-to-rule.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        repeat(n - i) {
+            print("  ")
+        }
+        for (j in 1..i) {
+            print("@ ")
+        }
+        println()
+    }
+    for (i in n - 1 downTo 1) {
+        for (j in 1..i) {
+            print("@ ")
+        }
+        println()
+    }
+}
