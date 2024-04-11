aece8cd271224b721eceec78b72046e7e43d94ac
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 13:32:10 +0900

    Title: 별표 출력하기 3, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/별표 출력하기 3/print-star-3.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in n downTo 1) {
+        repeat(n - i) {
+            print("  ")
+        }
+
+        for (j in 1..i * 2 - 1) {
+            print("* ")
+        }
+        println()
+    }
+}
