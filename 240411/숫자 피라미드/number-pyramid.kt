f4177580916fa4fa00d9ad990f42551aeac4d527
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 11:45:27 +0900

    Title: 숫자 피라미드, Time: 158ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240411/숫자 피라미드/number-pyramid.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        for (j in 1..i) {
+            print("$i ")
+        }
+        println()
+    }
+}
