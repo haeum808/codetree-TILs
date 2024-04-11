4c8e9760ad35070ae23962cd702110f1c47d4617
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 13:05:40 +0900

    Title: 특이한 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/특이한 출력/special-print.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        for (j in 1..n) {
+            print("($i, $j) ")
+
+            if ((i + j) % 4 == 0) println()
+        }
+    }
+}
