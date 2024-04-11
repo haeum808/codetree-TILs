6cde5fe926de550bbe5ca175c5c44dda06bbe729
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-11 11:10:13 +0900

    Title: 문자열 놀이, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240411/문자열 놀이/play-with-string.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val s = sc.next().toCharArray()
+    val q = sc.nextInt()
+
+    repeat(q) {
+        val num = sc.nextInt()
+        val a = sc.next()[0]
+        val b = sc.next()[0]
+
+        if (num == 1) {
+            val temp = s[a - '0' - 1]
+            s[a - '0' - 1] = s[b - '0' - 1]
+            s[b - '0' - 1] = temp
+        } else {
+            for (index in s.indices) {
+                if (s[index] == a) {
+                    s[index] = b
+                }
+            }
+        }
+        println(s.joinToString(""))
+    }
+
+}
