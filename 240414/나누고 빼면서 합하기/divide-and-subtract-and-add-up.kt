0455b7976080fb2af31dded4d233f2a15484d5ab
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 22:36:37 +0900

    Title: 나누고 빼면서 합하기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240414/나누고 빼면서 합하기/divide-and-subtract-and-add-up.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+var n = 0
+var m = 0
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    n = sc.nextInt()
+    m = sc.nextInt()
+    val a = Array(n) { sc.nextInt() }
+
+    change(a)
+
+    print(n)
+}
+
+fun change(a: Array<Int>) {
+    while(m != 1) {
+        n += a[m - 1]
+
+        if (m % 2 != 0) {
+            m -= 1
+        } else {
+            m /= 2
+        }
+    }
+}
