220a0cfc48dca994d926b8827292855dfea144a3
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 22:40:50 +0900

    Title: 나누고 빼면서 합하기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240414/나누고 빼면서 합하기/divide-and-subtract-and-add-up.kt
+++ 240414/나누고 빼면서 합하기/divide-and-subtract-and-add-up.kt
@@ -2,6 +2,7 @@ import java.util.Scanner
 
 var n = 0
 var m = 0
+var result = 0
 
 fun main() {
     val sc = Scanner(System.`in`)
@@ -12,12 +13,12 @@ fun main() {
 
     change(a)
 
-    print(n)
+    print(result)
 }
 
 fun change(a: Array<Int>) {
-    while(m > 1) {
-        n += a[m - 1]
+    while(m >= 1) {
+        result += a[m - 1]
 
         if (m % 2 != 0) {
             m -= 1
