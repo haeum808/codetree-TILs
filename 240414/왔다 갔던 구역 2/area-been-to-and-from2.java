cbe0143434e8ffbdfcbf4d83304a0911f021f9da
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 22:23:30 +0900

    Title: 왔다 갔던 구역 2, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -9,54 +9,30 @@ public class Main {
         for(int i=0;i<2001;i++) {
             arr[i] = 0;
         }
-        for(int i=0;i<101;i++) {
-            directions[i] = ' ';
-        }
+
         int start = 1000;
         for(int i=0;i<n;i++){
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
-            directions[i] = dir;
-            if(i>1) {
-                if (dir == 'R' && directions[i - 1] == 'L') {
-                    for (int j = start; j <= start + x && j < 2001; j++) {
-                        arr[j]++;
-                    }
-                    start += x;
-                } else if (dir == 'R' && directions[i - 1] == 'R') {
-                    for (int j = start + 1; j <= start + x && j < 2001; j++) {
-                        arr[j]++;
-                    }
-                    start += x;
-                } else if (dir == 'L' && directions[i - 1] == 'R') {
-                    for (int j = start; j >= start - x && j > 0; j--) {
-                        arr[j]++;
-                    }
-                    start -= x;
-                } else {
-                    for (int j = start - 1; j >= start - x && j > 0; j--) {
-                        arr[j]++;
-                    }
-                    start -= x;
+
+            if (dir == 'R') {
+                for (int j = start; j < start + x && j < 2001; j++) {
+                    arr[j]++;
                 }
-            } else {
-                if(dir == 'R') {
-                    for(int j=start;j<=start+x && j<2001;j++) {
-                        arr[j]++;
-                    }
-                    start += x;
-                } else {
-                    for(int j=start;j>=start-x && j>0;j--) {
-                        arr[j]++;
-                    }
-                    start -= x;
+                start += x;
+            }else {
+                for (int j = start - 1; j >= start - x && j > 0; j--) {
+                    arr[j]++;
                 }
+                start -= x;
             }
+
         }
         int count = 0;
         for(int i=0;i<2000;i++) {
-            if(arr[i] >=2 && arr[i+1] >= 2)
+            if(arr[i] >=2){
                 count++;
+            }
         }
         System.out.println(count);
     }
