a07cd0368aeff7e32a0c577409a04b5d4606c2a5
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 12:44:07 +0900

    Title: 2021년 날짜의 유무, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240414/2021년 날짜의 유무/with-or-without-2021.kt
@@ -0,0 +1,31 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val month = sc.nextInt()
+    val day = sc.nextInt()
+
+    if(determin2021(month, day)) {
+        print("Yes")
+    } else {
+        print("No")
+    }
+}
+
+fun determin2021(month: Int, day: Int): Boolean {
+    return when(month) {
+        1 -> { day in 1..31 }
+        2 -> { day in 1..28 }
+        3 -> { day in 1..31 }
+        4 -> { day in 1..30 }
+        5 -> { day in 1..31 }
+        6 -> { day in 1..30 }
+        7 -> { day in 1..31 }
+        8 -> { day in 1..31 }
+        9 -> { day in 1..30 }
+        10 -> { day in 1..31 }
+        11 -> { day in 1..30 }
+        else -> { day in 1..31 }
+    }
+}
