278e17c236de96104bfe716a171c430cbdb2d552
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 22:02:37 +0900

    Title: 왔다 갔던 구역 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,24 +5,52 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Integer[] arr = new Integer[2001];
+        char[] directions = new char[101];
         for(int i=0;i<2001;i++) {
             arr[i] = 0;
+        }
+        for(int i=0;i<101;i++) {
+            directions[i] = ' ';
         }
         int start = 1000;
         for(int i=0;i<n;i++){
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
-            if(dir =='R') {
-                for(int j=start;j<=start+x && j<2001;j++) {
-                    arr[j]++;
+            directions[i] = dir;
+            if(i>1) {
+                if (dir == 'R' && directions[i - 1] == 'L') {
+                    for (int j = start; j <= start + x && j < 2001; j++) {
+                        arr[j]++;
+                    }
+                    start += x;
+                } else if (dir == 'R' && directions[i - 1] == 'R') {
+                    for (int j = start + 1; j <= start + x && j < 2001; j++) {
+                        arr[j]++;
+                    }
+                    start += x;
+                } else if (dir == 'L' && directions[i - 1] == 'R') {
+                    for (int j = start; j >= start - x && j > 0; j--) {
+                        arr[j]++;
+                    }
+                    start -= x;
+                } else {
+                    for (int j = start - 1; j >= start - x && j > 0; j--) {
+                        arr[j]++;
+                    }
+                    start -= x;
                 }
-                start += x;
-            }
-            else {
-                for(int j=start;j>=start-x&&j>0;j--) {
-                    arr[j]++;
+            } else {
+                if(dir == 'R') {
+                    for(int j=start;j<=start+x && j<2001;j++) {
+                        arr[j]++;
+                    }
+                    start += x;
+                } else {
+                    for(int j=start;j>=start-x && j>0;j--) {
+                        arr[j]++;
+                    }
+                    start -= x;
                 }
-                start -= x;
             }
         }
         int count = 0;
