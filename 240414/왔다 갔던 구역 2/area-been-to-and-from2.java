83b60294c11ab025db1e2f7b2d58b813a8358bee
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-14 21:53:03 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240414/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -13,20 +13,21 @@ public class Main {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
             if(dir =='R') {
-                for(int j=start; j<Math.min(start+x, 2000); j++) { // 배열 범위를 초과하지 않도록 수정
-                     arr[j]++;
-                }
-                start = Math.min(start + x, 2000); // start 위치 업데이트 시 배열 범위를 초과하지 않도록 수정
-            } else {
-                for(int j=start; j>Math.max(start-x, 0); j--) { // 배열 범위를 초과하지 않도록 수정
+                for(int j=start;j<=start+x && j<2001;j++) {
                     arr[j]++;
                 }
-                start = Math.max(start - x, 0); // start 위치 업데이트 시 배열 범위를 초과하지 않도록 수정
+                start += x;
+            }
+            else {
+                for(int j=start;j>=start-x&&j>0;j--) {
+                    arr[j]++;
+                }
+                start -= x;
             }
         }
         int count = 0;
-        for(int i=0;i<2001;i++) {
-            if(arr[i] >=2)
+        for(int i=0;i<2000;i++) {
+            if(arr[i] >=2 && arr[i+1] >= 2)
                 count++;
         }
         System.out.println(count);
