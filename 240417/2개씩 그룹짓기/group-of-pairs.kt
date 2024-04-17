1fcdbb629baf7cf034a2f6191c6ba9577c648abb
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-17 11:14:35 +0900

    Title: 2개씩 그룹짓기, Time: 294ms, Memory: 18MB, Status: Passed - CodeTree

--- 240417/2개씩 그룹짓기/group-of-pairs.kt
+++ 240417/2개씩 그룹짓기/group-of-pairs.kt
@@ -3,8 +3,15 @@ import java.util.Scanner
 fun main() {
     val sc = Scanner(System.`in`)
 
+    var max = -1
     val n = sc.nextInt()
     val numbers = IntArray(n * 2) { sc.nextInt() }.sorted()
 
-    print(numbers[n] + numbers[n - 1])
+    for(i in 0..n - 1) {
+        if (max < numbers[i] + numbers[n * 2 - 1 - i]) {
+            max = numbers[i] + numbers[n * 2 - 1 - i]
+        }
+    }
+
+    print(max)
 }
