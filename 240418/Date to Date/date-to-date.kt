b3fc7ed4ba3fb7474f28d89648a637fc60d8ab24
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-18 21:08:02 +0900

    Title: Date to Date, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/Date to Date/date-to-date.kt
@@ -0,0 +1,31 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val m1 = sc.nextInt()
+    val d1 = sc.nextInt()
+    val m2 = sc.nextInt()
+    val d2 = sc.nextInt()
+
+    val result = (calculateMonthOfDay(m2) * 30 + d2) - (calculateMonthOfDay(m1) * 30 + d1) + 1
+
+    print(result)
+}
+
+fun calculateMonthOfDay(n: Int): Int {
+    return when(n) {
+        1 -> 31
+        2 -> 28
+        3 -> 31
+        4 -> 30
+        5 -> 31
+        6 -> 30
+        7 -> 31
+        8 -> 31
+        9 -> 30
+        10 -> 31
+        11 -> 30
+        else -> 31
+    }
+}
