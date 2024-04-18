47bed6826cefa7dd0f7be27c0647dcff0f854542
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-18 21:59:07 +0900

    Title: 그 요일은, Time: 152ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240418/그 요일은/the-day-of-the-day.kt
@@ -0,0 +1,43 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val m1 = sc.nextInt()
+    val d1 = sc.nextInt()
+    val m2 = sc.nextInt()
+    val d2 = sc.nextInt()
+    val a = sc.next()
+
+    var result1 = (1..m1 - 1).map { monthToDay(it) }.sum() + d1
+    val result2 = (1..m2 - 1).map { monthToDay(it) }.sum() + d2
+
+    when (a) {
+        "Mon" -> result1 -= 0
+        "Tue" -> result1 -= 1
+        "Wed" -> result1 -= 2
+        "Thu" -> result1 -= 3
+        "Fri" -> result1 -= 4
+        "Sat" -> result1 -= 5
+        else -> result1 -= 6
+    }
+
+    print((result2 - result1) / 7)
+}
+
+fun monthToDay(n: Int): Int {
+    return when(n) {
+        1 -> 31
+        2 -> 28
+        3 -> 31
+        4 -> 30
+        5 -> 31
+        6 -> 30
+        7 -> 31
+        8 -> 31
+        9 -> 30
+        10 -> 31
+        11 -> 30
+        else -> 31
+    }
+}
