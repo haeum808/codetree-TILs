e25b819224b3cc78e2f470418b48f1f5c20361cb
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-19 14:15:33 +0900

    Title: 왔다 갔던 구역 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240419/왔다 갔던 구역 2/area-been-to-and-from2.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    var curX = 0
+    val arr = IntArray(2001)
+
+    repeat(n) {
+        val x = sc.nextInt()
+        val direction = sc.next()
+
+        if (direction == "L") {
+            ((curX - x + 1000)..(curX + 1000)).forEach {
+                arr[it]++
+            }
+        } else {
+            ((curX + 1000)..(curX + x + 1000)).forEach {
+                arr[it]++
+            }
+        }
+
+        curX += x
+    }
+
+    print(arr.count { it >= 2 })
+}
