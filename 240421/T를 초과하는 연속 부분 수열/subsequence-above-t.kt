84f74bfcd33e492b3717275825dc36539978749d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-21 20:33:48 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 209ms, Memory: 12MB, Status: Passed - CodeTree

--- 240421/T를 초과하는 연속 부분 수열/subsequence-above-t.kt
+++ 240421/T를 초과하는 연속 부분 수열/subsequence-above-t.kt
@@ -11,7 +11,7 @@ fun main() {
     var max = 0
 
     for (i in numbers.indices) {
-        if (numbers[i] > k && (i == 0 || (numbers[i] > numbers[i - 1]))) {
+        if (numbers[i] > k) {
             cnt++
             max = max(max, cnt)
         } else {
@@ -19,7 +19,7 @@ fun main() {
         }
     }
 
-    if (max <= 1) {
+    if (max == 0) {
         print(0)
     } else {
         print(max)
