398eb87af47938321b3f071bdd81d1983409423e
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-21 20:04:52 +0900

    Title: 연속되는 수 4, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240421/연속되는 수 4/continuous-number4.kt
@@ -0,0 +1,22 @@
+import java.util.Scanner
+import kotlin.math.max
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    val numbers = IntArray(n) { sc.nextInt() }
+    var cnt = 0
+    var max = 0
+
+    for (i in numbers.indices) {
+        if (i == 0 || numbers[i] > numbers[i - 1]) {
+            cnt++
+        } else {
+            max = max(max, cnt)
+            cnt = 1
+        }
+    }
+
+    print(max)
+}
