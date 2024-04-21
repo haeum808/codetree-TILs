210bb60f71ce4c61bbda1cad883d0a849667ac0d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-21 20:00:09 +0900

    Title: 연속되는 수 3, Time: 188ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240421/연속되는 수 3/continuous-number3.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+import kotlin.math.max
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    val numbers = IntArray(n) { sc.nextInt() }
+    var cnt = 0
+    var max = 0
+
+    for (i in numbers.indices) {
+        if (i == 0 || numbers[i] * numbers[i - 1] > 0) {
+            cnt++
+        } else {
+            max = max(max, cnt)
+            cnt = 1
+        }
+    }
+    max = max(max, cnt)
+
+    print(max)
+}
