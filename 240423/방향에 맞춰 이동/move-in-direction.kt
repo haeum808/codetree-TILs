4161765beb30cb3f231319c976063cd51885ddfb
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-23 23:51:41 +0900

    Title: 방향에 맞춰 이동, Time: 156ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240423/방향에 맞춰 이동/move-in-direction.kt
@@ -0,0 +1,25 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    val dx = mapOf('W' to -1, 'S' to 0, 'N' to 0, 'E' to 1)
+    val dy = mapOf('W' to 0, 'S' to -1, 'N' to 1, 'E' to 0)
+
+    var nx = 0
+    var ny = 0
+
+    repeat(n) {
+        val direction = sc.next()[0]
+        val distance = sc.nextInt()
+
+        repeat(distance) {
+            nx += dx[direction]!!
+            ny += dy[direction]!!
+        }
+    }
+
+    print("$nx $ny")
+}
