440f5a5eacf8476bf52b07b02952cb7eada9393a
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-25 10:49:56 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 249ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240425/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.kt
@@ -0,0 +1,44 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    val m = sc.nextInt()
+
+    val arr2D = Array(n) { IntArray(m) }
+    var dirNum = 1
+
+    val dx = intArrayOf(0, 1, 0, -1)
+    val dy = intArrayOf(1, 0, -1, 0)
+
+    var x = 0
+    var y = 0
+
+    arr2D[0][0] = 1
+
+    for (i in 2..n * m) {
+        val nx = x + dx[dirNum]
+        val ny = y + dy[dirNum] 
+
+        if (!inRange(n, m, nx, ny) || arr2D[nx][ny] != 0) {
+            dirNum = (dirNum + 3) % 4
+        }
+
+        x += dx[dirNum]
+        y += dy[dirNum]
+
+        arr2D[x][y] = i
+    }
+
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            print("${arr2D[i][j]} ")
+        }
+        println()
+    }
+}
+
+fun inRange(n: Int, m: Int, x: Int, y: Int): Boolean {
+    return (x in 0 until n) && (y in 0 until m)
+}
