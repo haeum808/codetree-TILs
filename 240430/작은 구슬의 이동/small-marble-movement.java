ea973340dbe90a0197da1ec7938f747b33d25768
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-04-30 15:15:36 +0900

    Title: 작은 구슬의 이동, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240430/작은 구슬의 이동/small-marble-movement.java
+++ 240430/작은 구슬의 이동/small-marble-movement.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 public class Main {
 
-    static int[] dx = new int[]{1,0,-1,0}; // R D L U
-    static int[] dy = new int[]{0,-1,0,1};
+    static int[] dy = new int[]{1,0,-1,0};
+    static int[] dx = new int[]{0,-1,0,1};
     public static int dir(char c) {
-        if (c== 'R')
+        if(c=='R') // x좌표 증가
             return 0;
-        if (c== 'D')
+        if(c=='D') // y좌표 감소
             return 1;
-        if (c== 'L')
+        if(c=='L') //x좌표 감소
             return 2;
-        if (c== 'U')
+        if(c=='U') //y좌표 증가
             return 3;
         else
             return -1;
+    }
+    public static boolean isRange(int x, int y, int n) {
+        return 1<x && x<n && 1<y && y<n;
     }
 
     public static void main(String[] args) {
@@ -27,16 +30,14 @@ public class Main {
         char c = sc.next().charAt(0);
         int dir = dir(c);
         for(int i=0;i<t;i++) {
-            if(xpos ==1||xpos>n|| ypos ==1 ||ypos>n) {
-                i++;
+            if(xpos<1||xpos==n||ypos<1||ypos==n) {
                 dir = (dir+2)%4;
+                i--;
             }
             xpos += dx[dir];
             ypos += dy[dir];
         }
 
-        xpos -= 1;
-        ypos -= 1;
-        System.out.println(ypos + " "+ xpos);
+        System.out.println(xpos+ " "+ ypos);
     }
 }
