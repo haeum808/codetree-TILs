5d072a21aafcea9e4b25629f1dd0957daaff610d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-04 23:12:16 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240504/괄호 쌍 만들어주기 2/pair-parentheses-2.kt
+++ 240504/괄호 쌍 만들어주기 2/pair-parentheses-2.kt
@@ -6,17 +6,13 @@ fun main() {
     val input = sc.next()
     var result = 0
 
-    for (i in 0 until input.length) {
-        for (j in i + 1 until input.length) {
-            for (k in j + 1 until input.length) {
-                for (l in k + 1 until input.length) {
-                    if (input[i] == '(' && input[j] == '(' && i + 1 == j && k + 1 == l && input[k] == ')' && input[l] == ')') {
-                        result++
-                    }
-                }
+    for (i in 0 until input.length - 1) {
+        for (j in i + 2 until input.length - 1) {
+            if (input[i] == '(' && input[i + 1] == '(' && input[j] == ')' && input[j + 1] == ')') {
+                result++
             }
         }
     }
-    
+
     print(result)
 }
