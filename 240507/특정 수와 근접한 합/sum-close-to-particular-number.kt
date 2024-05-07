026b1a1593b80c098b933391e13b69375d247e26
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-07 12:11:51 +0900

    Title: 특정 수와 근접한 합, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240507/특정 수와 근접한 합/sum-close-to-particular-number.kt
+++ 240507/특정 수와 근접한 합/sum-close-to-particular-number.kt
@@ -8,18 +8,20 @@ fun main() {
     val n = sc.nextInt()
     val s = sc.nextInt()
     val numbers = IntArray(n) { sc.nextInt() }
-    var result = abs(s - numbers.filterIndexed { acc, idx -> idx != 0 && idx != n - 1  }.sum())
+    var result = Int.MAX_VALUE
 
     for (i in 0 until n - 1) {
-        var temp = 0
+        for (j in i + 1 until n) {
+            var temp = 0
 
-        for (j in 0 until n - 1) {
-            if (numbers[j] != numbers[i] && numbers[j] != numbers[i + 1]) {
-                temp += numbers[j]
+            for (k in 0 until n) {
+                if (k != i && k != j) {
+                    temp += numbers[k]
+                }
             }
-        }
 
-        result = min(result, abs(s - temp))
+            result = min(result, abs(s - temp))
+        }
     }
 
     print(result)
