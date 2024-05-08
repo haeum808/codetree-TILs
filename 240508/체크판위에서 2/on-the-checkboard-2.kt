c42de197729100e88166077333a3a4d9cf3dd1ee
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-08 14:06:22 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240508/체크판위에서 2/on-the-checkboard-2.kt
+++ 240508/체크판위에서 2/on-the-checkboard-2.kt
@@ -7,39 +7,47 @@ var c = 0
 fun main() {
     val sc = Scanner(System.`in`)
 
-    // 세로
+    // 세로변
     r = sc.nextInt()
-    // 가로
+    // 가로변
     c = sc.nextInt()
 
-    arr2D = Array(c) { CharArray(r) { sc.next()[0] } }
+    arr2D = Array(r) { CharArray(c) { sc.next()[0] } }
 
-    print(count(arr2D[0][0]))
+    if (arr2D[0][0] == 'W' && arr2D[r - 1][c - 1] == 'B') {
+        print(count('W'))
+    } else if (arr2D[0][0] == 'B' && arr2D[r - 1][c - 1] == 'W') {
+        print(count('B'))
+    } else {
+        print(0)
+    }
 }
 
-fun count(firstTarget: Char): Int {
+fun count(secondTarget: Char): Int {
     var result = 0
 
-    val secondTarget = if (firstTarget == 'B') {
+    val firstTarget = if (secondTarget == 'B') {
         'W'
     } else {
         'B'
     }
 
-    for (i in 1 until c - 1) {
-        for (j in 1 until r - 1) {
-            for (k in 1 until c - 1) {
-                var count = 0
-                for (l in 1 until r - 1) {
-                    if (
-                        arr2D[i][j] == secondTarget &&
-                        i < k && j < l && 
-                        arr2D[k][l] == firstTarget
-                        ) {
-                        count++
+    for (i in 1 until r - 1) {
+        var count = 0
+
+        for (j in 1 until c - 1) {
+            if (arr2D[i][j] == firstTarget) count++ else continue
+
+            for (k in i + 1 until r - 1) {
+                
+                for (l in j + 1 until c - 1) {
+                    if (i < k && j < l && arr2D[k][l] == secondTarget) {
+                        
+                        if (count == 1) {
+                            result++
+                        }
                     }
                 }
-                result += count
             }
         }
     }
