921871ab5164a7f2bbfac531b36d9961283955d5
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-08 14:33:45 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240508/체크판위에서 2/on-the-checkboard-2.kt
+++ 240508/체크판위에서 2/on-the-checkboard-2.kt
@@ -13,36 +13,36 @@ fun main() {
     c = sc.nextInt()
 
     arr2D = Array(r) { CharArray(c) { sc.next()[0] } }
-
+    
     if (arr2D[0][0] == 'W' && arr2D[r - 1][c - 1] == 'B') {
-        print(count('W'))
-    } else if (arr2D[0][0] == 'B' && arr2D[r - 1][c - 1] == 'W') {
         print(count('B'))
+    } else if (arr2D[0][0] == 'B' && arr2D[r - 1][c - 1] == 'W') {
+        print(count('W'))
     } else {
         print(0)
     }
 }
 
-fun count(secondTarget: Char): Int {
+fun count(firstTarget: Char): Int {
     var result = 0
 
-    val firstTarget = if (secondTarget == 'B') {
+    val secondTarget = if (firstTarget == 'B') {
         'W'
     } else {
         'B'
     }
 
-    for (i in 1 until r - 1) {
-        var count = 0
+    for (i in 1 until c - 1) {
+        for (j in 1 until r - 1) {
+            var count = 0
 
-        for (j in 1 until c - 1) {
-            if (arr2D[i][j] == firstTarget) count++ else continue
+            if (arr2D[i][j] == firstTarget) count++
 
-            for (k in 1 until r - 1) {
-                
-                for (l in 1 until c - 1) {
-                    if (i < k && j < l && arr2D[k][l] == secondTarget) {
-                        
+            for (k in i + 1 until c - 1) {
+
+                for (l in j + 1 until r - 1) {
+                    if (arr2D[k][l] == secondTarget) {
+
                         if (count == 1) {
                             result++
                         }
