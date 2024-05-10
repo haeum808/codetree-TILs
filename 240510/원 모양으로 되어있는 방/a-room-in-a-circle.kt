7ffdf27a65577cd38a4602587c45c3c60611b65b
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-10 09:51:06 +0900

    Title: 원 모양으로 되어있는 방, Time: 206ms, Memory: 13MB, Status: Passed - CodeTree

--- 240510/원 모양으로 되어있는 방/a-room-in-a-circle.kt
+++ 240510/원 모양으로 되어있는 방/a-room-in-a-circle.kt
@@ -1,32 +1,20 @@
 import java.util.Scanner
 import kotlin.math.min
-import java.util.Queue
-import java.util.LinkedList
 
 fun main() {
     val sc = Scanner(System.`in`)
 
     val n = sc.nextInt()
-    val queue: Queue<Int> = LinkedList<Int>()
-
-    repeat(n) {
-        queue.offer(sc.nextInt())
-    }
+    val numbers = IntArray(n) { sc.nextInt() }
     var min = Int.MAX_VALUE
 
     for (i in 0 until n) {
         var sum = 0
-        queue.offer(queue.poll())
-
-        for (j in 1 until n) {
-            val poll = queue.poll()
-
-            sum += poll * j
-            queue.offer(poll)
+        for (j in 0 until n) {
+            val dist = (j + n - i) % n
+            sum += numbers[j] * dist
         }
         min = min(min, sum)
-
-        queue.offer(queue.poll())
     }
 
     print(min)
