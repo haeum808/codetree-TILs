dbd2a8e29e7e2b8ccf9119bd37019e5aa043f1c0
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-14 11:04:00 +0900

    Title: 오목, Time: 187ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240514/오목/O-mok.java
+++ 240514/오목/O-mok.java
@@ -42,7 +42,7 @@ public class Main {
         //대각선으로 이겼을 경우
         for(int i=0;i<14;i++) {
             for(int j=0;j<14;j++) {
-            
+
                 if(plate[i][j]==1 && plate[i+1][j+1]==1 && plate[i+2][j+2]==1 && plate[i+3][j+3]==1 && plate[i+4][j+4]==1){
                     x = i+2;
                     y = j+2;
@@ -57,9 +57,25 @@ public class Main {
                 }
             }
         }
+        for(int i=4;i<19;i++) {
+            for(int j=4;j<19;j++) {
+                if(plate[i][j]==1 & plate[i-1][j-1]==1 &&plate[i-2][j-2]==1 && plate[i-3][j-3]==1 && plate[i-4][j-4]==1) {
+                    x = i-2;
+                    y = j-2;
+                    winnerNum = 1;
+                    return true;
+                }
+                if(plate[i][j]==1 & plate[i-1][j-1]==2 &&plate[i-2][j-2]==2 && plate[i-3][j-3]==2 && plate[i-4][j-4]==2) {
+                    x = i-2;
+                    y = j-2;
+                    winnerNum = 2;
+                    return true;
+                }
+            }
+        }
         return false;
     }
- 
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -97,6 +113,6 @@ public class Main {
         } else {
             return;
         }
-        
+
     }
 }
