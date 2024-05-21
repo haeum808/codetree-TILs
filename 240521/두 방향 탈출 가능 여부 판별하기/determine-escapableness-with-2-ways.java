d9b4429f6cc45e787e36537ce014fab294770db4
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-05-21 21:07:09 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 308ms, Memory: 21MB, Status: Passed - CodeTree

--- 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -1,57 +1,52 @@
 import java.util.*;
 public class Main {
-    static int n, m;
-    static int[][] grid;
-    static boolean[][] visited;
-    static int[][] memo;
+    static int n;
+    static int m;
     static int[] dx = new int[]{1, 0};
     static int[] dy = new int[]{0, 1};
+    static int[][] grid;
+    static boolean[][] visited;
+    static int answer = 0;
+    static Queue<int[]> queue = new LinkedList<>();
 
-    public static boolean inRange(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < m;
+    public static boolean inRange(int x, int y){
+        return 0<=x && x<n && 0<=y && y<m;
     }
-
-    public static int dfs(int x, int y) {
-        if (x == n - 1 && y == m - 1) {
-            return 1;
-        }
-        if (memo[x][y] != -1) {
-            return memo[x][y];
-        }
-
-        int paths = 0;
-        for (int i = 0; i < 2; i++) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            if (inRange(newX, newY) && !visited[newX][newY] && grid[newX][newY] == 1) {
-                visited[newX][newY] = true;
-                paths += dfs(newX, newY);
-                visited[newX][newY] = false;
+    public static void bfs(int x, int y) {
+        queue.add(new int[]{x, y});
+        while(!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            int curX = curPos[0];
+            int curY = curPos[1];
+            if (curX == n - 1 && curY == m - 1) {
+                answer = 1;
+                return;
+            }
+            for (int i = 0; i < 2; i++) {
+                int newX = curX + dx[i];
+                int newY = curY + dy[i];
+                if (inRange(newX, newY) && !visited[newX][newY] && grid[newX][newY] == 1) {
+                    visited[newX][newY] = true;
+                    int[] newPos = new int[]{newX, newY};
+                    queue.add(newPos);
+                }
             }
         }
-
-        memo[x][y] = paths;
-        return paths;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
         m = sc.nextInt();
+
         grid = new int[n][m];
         visited = new boolean[n][m];
-        memo = new int[n][m];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++){
                 grid[i][j] = sc.nextInt();
-                memo[i][j] = -1; // 메모 배열을 초기화합니다.
             }
         }
-
-        visited[0][0] = true;
-        int answer = dfs(0, 0);
+        bfs(0, 0);
         System.out.println(answer);
     }
 }
