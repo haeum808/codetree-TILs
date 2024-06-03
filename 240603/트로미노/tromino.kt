fbe6fd25d8b42f20953ffb11cb9215682ba5ab8f
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-06-03 23:53:39 +0900

    Title: 트로미노, Time: 544ms, Memory: 25MB, Status: Passed - CodeTree

--- 240603/트로미노/tromino.kt
+++ 240603/트로미노/tromino.kt
@@ -9,11 +9,11 @@ data class Point(
 fun main() {
     val sc = Scanner(System.`in`)
 
-    val n = sc.nextInt()
-    val m = sc.nextInt()
+    val n = sc.nextInt() + 2
+    val m = sc.nextInt() + 2
     var result = 0
 
-    val arr2D = Array(n) { IntArray(m) { sc.nextInt() } }
+    val arr2D = Array(n) { i -> IntArray(m) { j -> if (i == 0 || j == 0 || i == n - 1 || j == m - 1) 0 else sc.nextInt() } }
 
     val dxdy = listOf(
         listOf(Point(0, -1), Point(0, 1)),
@@ -24,18 +24,15 @@ fun main() {
         listOf(Point(1, 1), Point(1, 0))
     )
 
-    for (i in 0 until n) {
-        for (j in 0 until m) {
+    for (i in 1 until n - 1) {
+        for (j in 1 until m - 1) {
             for (k in 0 until 6) {
                 val nx1 = i + dxdy[k][0].x
                 val ny1 = j + dxdy[k][0].y
                 val nx2 = i + dxdy[k][1].x
                 val ny2 = j + dxdy[k][1].y
 
-                if (nx1 in 0 until n && nx2 in 0 until n && ny1 in 0 until m && ny2 in 0 until m) {
-                    result = max(result, arr2D[i][j] + arr2D[nx1][ny1] + arr2D[nx2][ny2])
-                }
-                
+                result = max(result, arr2D[i][j] + arr2D[nx1][ny1] + arr2D[nx2][ny2])
             }
         }
     }
