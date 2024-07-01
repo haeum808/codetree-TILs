150bf84e41010df03d4de67493a90ca2f150789b
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-01 23:48:03 +0900

    Title: 기울어진 직사각형, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240701/기울어진 직사각형/slanted-rectangle.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] grid;
+    static boolean[][] visited;
+    static int n;
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    public static int move(int x, int y) {
+        int count = 0;
+        count += grid[x][y];
+        for (int num = 0; num < 4; num++) {
+            int tempX = x, tempY = y;
+            while (true) {
+                if (num == 0) {
+                    tempX = x - 1;
+                    tempY = y + 1;
+                } else if (num == 1) {
+                    tempX = x - 1;
+                    tempY = y - 1;
+                } else if (num == 2) {
+                    tempX = x + 1;
+                    tempY = y - 1;
+                } else {
+                    tempX = x + 1;
+                    tempY = y + 1;
+                }
+
+                if (inRange(tempX, tempY) && !visited[tempX][tempY]) {
+                    x = tempX;
+                    y = tempY;
+                    visited[x][y] = true;
+                    count += grid[x][y];
+                } else {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void resetVisited() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    public static int check() {
+        int max = 0;
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                resetVisited();
+                visited[i][j] = true;
+                int count = move(i, j);
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        grid = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        int max = check();
+        System.out.println(max);
+    }
+}
