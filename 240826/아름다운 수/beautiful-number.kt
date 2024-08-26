fbea8363a0cd5c712283a71416520fdeb5eefd47
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-26 14:23:46 +0900

    Title: 아름다운 수, Time: 265ms, Memory: 14MB, Status: Passed - CodeTree

--- 240826/아름다운 수/beautiful-number.kt
+++ 240826/아름다운 수/beautiful-number.kt
@@ -5,19 +5,20 @@ var count = 0
 fun main() {
     n = readLine()!!.toInt()
     choose(0)
-    println(count)
+    print(count)
 }
 
 fun check() {
-    var index = 0
+    var i = 0
 
-    while (index < n) {
-        when (numbers[index]) {
-            1 -> index++
-            2 -> if (index + 1 < n && numbers[index + 1] == 2) index += 2 else return
-            3 -> if (index + 2 < n && numbers[index + 1] == 3 && numbers[index + 2] == 3) index += 3 else return
-            4 -> if (index + 3 < n && numbers[index + 1] == 4 && numbers[index + 2] == 4 && numbers[index + 3] == 4) index += 4 else return
+    while (i < n) {
+        if (i + numbers[i] - 1 >= n) return
+
+        for (j in i until i + numbers[i]) {
+            if (numbers[j] != numbers[i]) return
         }
+
+        i += numbers[i]
     }
 
     count++
