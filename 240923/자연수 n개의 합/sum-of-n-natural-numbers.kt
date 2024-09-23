ddbb17b045ebfa225fa2545718eb4b59aaf07d7d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-23 22:25:47 +0900

    Title: 자연수 n개의 합, Time: 110ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240923/자연수 n개의 합/sum-of-n-natural-numbers.kt
+++ 240923/자연수 n개의 합/sum-of-n-natural-numbers.kt
@@ -1,17 +1,17 @@
 fun main() {
     val s = readln().toLong()
-    var left = 1
+    var left = 1L
     var right = s
     var max = 0L
 
-    while (left < right) {
+    while (left <= right) {
         val mid = (left + right) / 2L
 
         if (mid * (mid + 1) / 2 <= s) {
-            left += 1
+            left = mid + 1
             max = maxOf(mid, max)
         } else {
-            right -= 1
+            right = mid - 1
         }
     }
 
