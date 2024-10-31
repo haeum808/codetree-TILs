fcb713e6b96c79ad384dc4bf64adf0aea2ae6bb8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-31 22:09:02 +0900

    Title: 겹치지 않게 선분 고르기, Time: 280ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241031/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] lines = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+
+            lines[i][0] = x1;
+            lines[i][1] = x2;
+        }
+        Arrays.sort(lines, Comparator.comparingInt(a -> a[1]));
+
+        int answer = 0;
+        int lastElement = 0;
+        for (int i = 0; i < n; i++) {
+            if (lines[i][0] >= lastElement) {
+                answer++;
+                lastElement = lines[i][1];
+            }
+        }
+        System.out.println(answer);
+    }
+}
