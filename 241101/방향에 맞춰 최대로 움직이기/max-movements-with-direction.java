71505207e1b16baa10119526a0be3f8ab2d11290
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-01 22:52:16 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241101/방향에 맞춰 최대로 움직이기/max-movements-with-direction.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] arr;
+    static int[][] directions;
+    static int[] dx = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = new int[]{0, 1, 1, 1, 0, -1, -1, -1};
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                int[] curPos = queue.poll();
+                int curx = curPos[0];
+                int cury = curPos[1];
+                int dir = directions[curx][cury];
+
+                for (int time = 1;;time++){
+                    int nx = curx + dx[dir - 1] * time;
+                    int ny = cury + dy[dir - 1] * time;
+                    if (!inRange(nx, ny)) break;
+                    if (arr[nx][ny] <= arr[curx][cury]) break;
+
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+            count++;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //ㄷㅓ 큰 숫자로 이동하는 것이 가능하도록
+        n = sc.nextInt();
+        arr = new int[n][n];
+        directions = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                directions[i][j] = sc.nextInt();
+            }
+        }
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int answer = bfs(r-1, c-1);
+        System.out.println(answer);
+
+    }
+}
