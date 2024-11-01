8dfa1d74b802954dd365f123f4d00891194f265a
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-01 23:23:43 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241101/방향에 맞춰 최대로 움직이기/max-movements-with-direction.java
+++ 241101/방향에 맞춰 최대로 움직이기/max-movements-with-direction.java
@@ -1,67 +1,68 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int[][] arr;
-    static int[][] directions;
-    static int[] dx = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = new int[]{0, 1, 1, 1, 0, -1, -1, -1};
+    public static final int MAX_N = 4;
+
+    public static int n;
+    public static int[][] num = new int[MAX_N][MAX_N];
+    public static int[][] moveDir = new int[MAX_N][MAX_N];
+    public static int ans;
 
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < n;
     }
 
-    public static int bfs(int x, int y) {
+    public static boolean canGo(int x, int y, int prevNum) {
+        return inRange(x, y) && num[x][y] > prevNum;
+    }
+
+    public static void findMax(int startX, int startY) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        int count = 0;
-        if (n == 1) {
-            return 0;
-        }
+        queue.offer(new int[]{startX, startY, 0}); // {x, y, cnt}
+        ans = 0;
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int s = 0; s < size; s++) {
-                int[] curPos = queue.poll();
-                int curx = curPos[0];
-                int cury = curPos[1];
-                int dir = directions[curx][cury];
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int cnt = current[2];
 
-                for (int time = 1;;time++){
-                    int nx = curx + dx[dir - 1] * time;
-                    int ny = cury + dy[dir - 1] * time;
-                    if (!inRange(nx, ny)) break;
-                    if (arr[nx][ny] <= arr[curx][cury]) break;
+            ans = Math.max(ans, cnt);
 
-                    queue.add(new int[]{nx, ny});
+            int[] dx = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
+            int[] dy = new int[]{0, 1, 1, 1, 0, -1, -1, -1};
+            int d = moveDir[x][y] - 1;
+
+            for (int i = 1; i < n; i++) { // i는 1부터 시작
+                int nx = x + dx[d] * i;
+                int ny = y + dy[d] * i;
+                if (canGo(nx, ny, num[x][y])) {
+                    queue.add(new int[]{nx, ny, cnt + 1});
+                } else {
+                    break; // 더 이상 진행할 수 없으므로 종료
                 }
             }
-            count++;
         }
-
-        return count;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //ㄷㅓ 큰 숫자로 이동하는 것이 가능하도록
         n = sc.nextInt();
-        arr = new int[n][n];
-        directions = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                directions[i][j] = sc.nextInt();
-            }
-        }
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                num[i][j] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                moveDir[i][j] = sc.nextInt();
+
         int r = sc.nextInt();
         int c = sc.nextInt();
 
-        int answer = bfs(r-1, c-1);
-        System.out.println(answer);
-
+        findMax(r - 1, c - 1);
+        System.out.print(ans);
     }
 }
